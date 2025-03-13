ca385a902b33d710e44d1009bed096c33d2d7788
atharva-555 <atharva.bobade555@gmail.com>
2025-03-14 00:36:29 +0530

    Footer deisgn 1.0

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import'./footer.css'
+import Logo from '../../assets/images/Logo.svg'
+import MapImg from '../../assets/images/map.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="row px-2 py-4">
+
+        <div className="col-3 d-flex flex-column justify-content-center align-items-center part1">
+          <div className="footer-logo"><img src={Logo} alt="" /></div>
+          <div className="clgName">
+            <h3>Padmabhooshan Vasantdada Patil Institute of Technology</h3>
+          </div>
+        </div>
+
+        <div className="col-3 part2 ">
+          <div className="heading">
+            <h2 className='px-2'>Address</h2>
+            <hr style={{ border: "2px solid white", width: "100%" ,opacity:1,margin:'0',marginBottom:'5px'}} />
+            <p className='py-2'>S No 33/22, Pune Pirangut Road, next to Chandani Chowk, Bavdhan, Pune, Maharashtra 411021.</p>
+            <img className='py-2 px-2 mapImg' src={MapImg} alt="" /> 
+            <p className='pt-2 text-center'>Get Directions</p>        
+          </div>
+        </div>
+
+        <div className="col-3 part3 ">
+          <div className="heading">
+              <h2 className='px-2'>Connect with Us</h2>
+              <hr style={{ border: "2px solid white", width: "100%" ,opacity:1,margin:'0',marginBottom:'5px'}} />
+              <p className='py-2'>S No 33/22, Pune Pirangut Road, next to Chandani Chowk, Bavdhan, Pune, Maharashtra 411021.</p>
+          </div>
+        </div>
+        <div className="col-3 "></div>
+      </div>
+    </footer>
   )
 }
 
--- src/Components/navbar/MainPagesNavbar.jsx
+++ src/Components/navbar/MainPagesNavbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import './mainPagesNavbar.css'
 import { useState } from 'react';
+import Logo from '../../assets/images/Logo.svg'
 
 const MainPagesNavbar = () => {
 
@@ -13,16 +14,18 @@ const MainPagesNavbar = () => {
 
   return (
     <header>
-      <div className = "row px-4 m-0">
-        <div className = "col-4 d-flex justify-content-center align-items-center part1">
-          <Link to='/'><img className="logo" alt="" />LOGO</Link>
+      <div className = "row p-2 m-0">
+        <div className = "col-5 d-flex justify-content-center align-items-center part1 m-auto">
+          <Link to='/'><img className="nav-logo" alt="" src={Logo}/>PVPIT CONNECT</Link>
         </div>
-        <div className="col-8 d-flex align-items-center part2">
+        <div className="col-7 d-flex align-items-center part2">
           <ul className="list list-inline mb-0">
-            <li className="list-inline-item"><Link to ="/"   className={`${activeLink === 'home' ? 'active' : ''}`} onClick={() => handleLinkClick('home')} aria-current="home">Home</Link></li>
-            <li className="list-inline-item"><Link to ="about" className={`${activeLink === 'about' ? 'active':''}`} onClick={()=>{handleLinkClick('about')}}>About Us</Link></li>
-            <li className="list-inline-item"><Link to ="services" className={`${activeLink === 'services' ? 'active':''}`} onClick={()=>{handleLinkClick('services')}}>Services</Link></li>
-            <li className="list-inline-item"><Link to ="contact" className={`${activeLink === 'contact' ? 'active':''}`} onClick={()=>{handleLinkClick('contact')}}>Contact</Link></li>
+            {/* <li className="list-inline-item"><Link to ="/"   className={`${activeLink === 'home' ? 'active' : ''}`} onClick={() => handleLinkClick('home')} aria-current="home">Home</Link></li> */}
+            <li className="list-inline-item"><Link to ="about" className={`${activeLink === 'about' ? 'active':''}`} onClick={()=>{handleLinkClick('about')}}>Community</Link></li>
+            <li className="list-inline-item"><Link to ="about" className={`${activeLink === 'about' ? 'active':''}`} onClick={()=>{handleLinkClick('about')}}>Events</Link></li>
+            <li className="list-inline-item"><Link to ="about" className={`${activeLink === 'about' ? 'active':''}`} onClick={()=>{handleLinkClick('about')}}>About</Link></li>
+            <li className="list-inline-item"><Link to ="services" className={`${activeLink === 'services' ? 'active':''}`} onClick={()=>{handleLinkClick('services')}}>Login</Link></li>
+            <li className="list-inline-item"><Link to ="contact" className={`${activeLink === 'contact' ? 'active':''}`} onClick={()=>{handleLinkClick('contact')}}>Register</Link></li>
           </ul>
         </div>
       
